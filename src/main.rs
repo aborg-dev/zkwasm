525c62d564ca2fb2792b57e2b0bde4fac3264fb7
Andrei Kashin <kashin.andrej@gmail.com>
2023-08-11 17:15:46 +0100

    Add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use std::{env, fs};
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: main WASM_FILEPATH");
-        return;
-    }
-    let module = fs::read_to_string(&args[1]).expect("Failed to read file");
+use anyhow::Result;
+
+fn parse(module: &[u8]) -> Result<()> {
     let parser = wasmparser::Parser::new(0);
     
-    for payload in parser.parse_all(module.as_bytes()) {
-        dbg!(payload.expect("Why result?"));
+    for payload in parser.parse_all(module) {
+        let payload = payload?;
+        dbg!(payload);
     }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        anyhow::bail!("Usage: main WASM_FILEPATH");
+    }
+    let module = fs::read_to_string(&args[1]).expect("Failed to read file");
+    parse(module.as_bytes())?;
+    Ok(())
 }
